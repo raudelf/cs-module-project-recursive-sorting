f0b5828d046dc26725144d3e90fc57ab937a1227
raudelf <rawdell@outlook.com>
2020-10-05 21:17:47 -0500

    :art: sorting.py
    Fixed issues with the helper function where I was getting an out of range index. Once fixed I was able to get the merge sort test working to meet MVP

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -5,40 +5,57 @@ def merge(arrA, arrB):
 
     # Your code here
 
-    while elements >= 2:
-        left = arrA[0]
-        right = arrB[0]
+    while elements:
 
-        if left < right:
-            arrA.pop(0)
-            merged_arr.append(left)
-        if right < left:
-            arrB.pop(0)
-            merged_arr.append(right)
-        
-        elements -= 1
-    
-    if arrA:
-        merged_arr.append(arrA)
-    else:
-        merged_arr.append(arrB)
+        if arrA and arrB:
+            if arrA[0] < arrB[0]:
+                merged_arr.append(arrA[0])
+                arrA.pop(0)
+                elements -= 1
+            else:
+                merged_arr.append(arrB[0])
+                arrB.pop(0)
+                elements -= 1
+        else:
+            if arrA:
+                merged_arr.append(arrA[0])
+                arrA.pop(0)
+                elements -= 1
+            if arrB:
+                merged_arr.append(arrB[0])
+                arrB.pop(0)
+                elements -= 1
 
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
+
+
 def merge_sort(arr):
     # Your code here
+    if len(arr) > 1:
+        middle = len(arr) // 2
+        left = arr[:middle]
+        right = arr[middle:]
 
+        left = merge_sort(left)
+        right = merge_sort(right)
 
-    return arr
+        arr = merge(left, right)
+        return arr
+    else:
+        return arr
 
-# STRETCH: implement the recursive logic for merge sort in a way that doesn't 
+# STRETCH: implement the recursive logic for merge sort in a way that doesn't
 # utilize any extra memory
-# In other words, your implementation should not allocate any additional lists 
+# In other words, your implementation should not allocate any additional lists
 # or data structures; it can only re-use the memory it was given as input
+
+
 def merge_in_place(arr, start, mid, end):
     # Your code here
     pass
+
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
